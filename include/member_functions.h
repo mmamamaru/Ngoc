bebfce8e7960f5533fa826bec74a71262ccd6962
Peter Le <duclocle.work@gmail.com>
2024-09-03 12:29:26 +0700

    New changes for file configuration and include path error fixed!

--- include/member_functions.h
+++ include/member_functions.h
@@ -2,3 +2,33 @@
 This file includes calling functions and include header functions
 */
 #include <iostream>
+
+using std::string, std::cout, std::cin;
+
+class Member{
+    private:
+        string name;
+        string password;
+        string fullname;
+        string email;
+        int IDnum;
+    protected:
+        int phoneNum;
+        int creditPoints;
+    public:
+        // Constructors:
+
+
+        // Methods:
+        int getCredit(){
+            return creditPoints;
+        }
+
+        void purchaseCredit(){
+            int creditPurchase;
+            cout << "How many credit you want to purchase: "; cin >> creditPurchase;
+            creditPoints = creditPurchase;
+            cout << "Purchased credit successfully!\n";
+        }
+
+};
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 /*
 This file is the main file function, allows the user to interact with the program by running this file!
 */
+#include "include/member_functions.h"
 #include <iostream>
 
 int main()
