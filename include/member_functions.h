049cd1523287e7caa30965302e31ee82249cff0e
PeterLe611 <142322089+PeterLe611@users.noreply.github.com>
2024-09-09 21:08:56 +0700

    A few more necessary files for debugging and changes to member functions

--- include/member_functions.h
+++ include/member_functions.h
@@ -6,29 +6,50 @@ This file includes calling functions and include header functions
 using std::string, std::cout, std::cin;
 
 class Member{
-    private:
-        string name;
-        string password;
-        string fullname;
-        string email;
-        int IDnum;
-    protected:
-        int phoneNum;
-        int creditPoints;
-    public:
-        // Constructors:
+private:
+    string name;
+    string password;
+    string fullname;
+    string email;
+    int IDnum;
+protected:
+    int phoneNum;
+    int creditPoints;
+public:
+    // Constructors:
 
 
-        // Methods:
-        int getCredit(){
-            return creditPoints;
-        }
+    // Methods:
+    int getCredit(){
+        return creditPoints;
+    }
 
-        void purchaseCredit(){
-            int creditPurchase;
-            cout << "How many credit you want to purchase: "; cin >> creditPurchase;
-            creditPoints = creditPurchase;
-            cout << "Purchased credit successfully!\n";
-        }
+    void purchaseCredit(){
+        int creditPurchase;
+        cout << "How many credit you want to purchase: "; cin >> creditPurchase;
+        creditPoints = creditPurchase;
+        cout << "Purchased credit successfully!\n";
+    }
+};
 
+class User:public Member{
+private:
+    scoreRating passengerScore;
+
+};
+
+class Drivers:public Member{
+private:
+    scoreRating driverScore;
+
+};
+
+class scoreRating{
+private:
+    int score;
+
+public:
+    void setScore(int newScore){
+        score = newScore;
+    }
 };
